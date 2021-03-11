a2237efa678b2a360996c269638635cb1868ac09
Takagi Kensuke <janus.wel.3@gmail.com>
2021-03-11 18:29:39 +0900

    :sparkles: modeling for coins and bills

---
+++ src/domain/valueobjects/money.ts
@@ -0,0 +1,29 @@
+export class TenYenCoin {
+  valueOf() {
+    return 10
+  }
+}
+
+export class FiftyYenCoin {
+  valueOf() {
+    return 50
+  }
+}
+
+export class HundredYenCoin {
+  valueOf() {
+    return 100
+  }
+}
+
+export class FiveHundredYenCoin {
+  valueOf() {
+    return 500
+  }
+}
+
+export class ThousandYenBill {
+  valueOf() {
+    return 1000
+  }
+}
